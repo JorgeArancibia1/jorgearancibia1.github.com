b7c48172ad3ecb0adcdc201cbe6097fdb266ce8e
Jorge Arancibia <jorge.arancibia.jeal@gmail.com>
2020-01-12 03:14:45 -0300

    Updates

--- PreloadTest/index.js
+++
@@ -1,9 +0,0 @@
-function loadImage() {
-    var preload = new createjs.LoadQueue();
-    preload.addEventListener("fileload", handleFileComplete);
-    preload.loadFile("MongoDB.png");
-  }
-
-  function handleFileComplete(event) {
-    document.body.appendChild(event.result);
-  }
